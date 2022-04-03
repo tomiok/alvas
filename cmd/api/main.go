a0df30686eca8353b8a0c943da1b123f442f7a3e
tomas lingotti <tomas.lingotti@mercadolibre.com>
2022-04-03 13:30:53 -0300

    cleanup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/tomiok/alvas/internal/database"
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Info().Msg("application starting")
 	database.Init()
 }
