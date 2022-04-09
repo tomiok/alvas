02dae7c19f010faedff34440acca573f814b886a
tomas lingotti <tomas.lingotti@mercadolibre.com>
2022-04-09 19:17:30 -0300

    add fileserver

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,10 +15,15 @@ import (
 func routesSetup(db *gorm.DB, sess *scs.SessionManager) chi.Router {
 	r := chi.NewRouter()
 
+	// middlewares
 	r.Use(middleware.Recoverer)
 	r.Use(csrfmid.NoSurf())
 	r.Use(sessmid.LoadSession)
 
+	// file server
+	fileServer(r)
+
+	// application routes
 	customerRoutes(db, r, sess)
 	pingRoute(r)
 	homeRoute(r)
@@ -40,4 +45,9 @@ func pingRoute(r chi.Router) {
 
 func homeRoute(r chi.Router) {
 	r.Get("/", home.RenderView)
+}
+
+func fileServer(r *chi.Mux) {
+	fs := http.FileServer(http.Dir("./static/"))
+	r.Handle("/static/*", fs)
 }
